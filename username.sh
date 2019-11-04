fda33658d70ac6408a0065009d6296b4cb1c8cb5
Parsa Hassani <mohamad.parsa.hasani@gmail.com>
2019-11-04 10:46:13 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -2,7 +2,7 @@
 # username.sh
 echo "Enter your username.: "
 read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$ZIP" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - Try again!"
 	echo "Enter a username that starts with a lower case letter that is atleast three, but no more than 12 characters. Username: "
